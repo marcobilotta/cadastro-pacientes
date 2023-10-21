48d5b1c575ebc2c3fb5b77876fd07019fb1d4440
Marco Bilotta <marco.bilotta@outlook.com>
2023-10-21 12:41:16 -0300

    fix: initial commit

---
+++ src/main/java/br/com/cadastropacientes/CadastroPacientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastropacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPacientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastropacientes/CadastroPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastropacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
